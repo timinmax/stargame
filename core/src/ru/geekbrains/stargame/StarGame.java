223b3203c61b6d9db8d144acea215bbd4fa7de29
Maxim Timinskiy <timinmax@gmail.com>
2020-04-25 17:26:19 +0300

    static background ready

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -5,22 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Texture bckGrnd;
+	private Texture texture;
+	private TextureRegion region;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		bckGrnd = new Texture("bkgrnd2.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.draw(bckGrnd, 0, 0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
@@ -29,5 +35,6 @@ public class StarGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		bckGrnd.dispose();
 	}
 }
