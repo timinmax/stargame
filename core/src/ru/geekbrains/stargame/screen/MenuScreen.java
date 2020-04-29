bfc047a1864a0189c1cf61ad3dcd837bc13213f2
Maxim Timinskiy <timinmax@gmail.com>
2020-04-29 22:24:52 +0300

    refactor ready

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -2,6 +2,7 @@ package ru.geekbrains.stargame.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.base.BaseScreen;
 
@@ -9,22 +10,37 @@ public class MenuScreen extends BaseScreen {
     Texture img;
     Texture bckGrnd;
 
+    private Vector2 pos,v,gravity,touch;
+
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         bckGrnd = new Texture("bkgrnd2.jpg");
 
+        pos = new Vector2();
+        touch = new Vector2();
+        v = new Vector2(1,1);
+        gravity = new Vector2(0,-0.005f);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-
+        pos.add(v);
+        v.add(gravity);
         batch.begin();
         batch.draw(bckGrnd, 0, 0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(img, 0, 0);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY );
+        pos.set(touch);
+        v.set(1,1);
+        return super.touchDown(screenX, screenY, pointer, button);
     }
 
     @Override
